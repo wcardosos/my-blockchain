774fcfe74d65a57f2e43a377e1711a1e385c6d7d
Wagner Cardoso <wagnerdev01@gmail.com>
2024-01-04 12:34:30 -0300

    Initial project

---
+++ blockchain/blockchain.py
@@ -0,0 +1,55 @@
+import hashlib
+import json
+from time import time
+from typing import Optional
+
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self, proof: int, previous_hash: Optional[str] = None) -> dict:
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender: str, recipient: str, amount: int) -> int:
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+
+        return self.last_block['index'] + 1
+
+    def proof_of_work(self, last_proof: int) -> int:
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof: int, proof: int) -> bool:
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+    @staticmethod
+    def hash(block: dict) -> str:
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    @property
+    def last_block(self) -> dict:
+        return self.chain[-1]
